1f927940b0f6b08ba599364730575251833e8b94
CarrotIfson <didacysusana@gmail.com>
2022-10-16 21:18:26 +0200

    Refactored array to ints

--- contracts/BytesCGoL2P.sol
+++ contracts/BytesCGoL2P.sol
@@ -9,9 +9,7 @@ contract BGCOL {
     struct Game {
         uint256 rows;
         uint256 cols;
-        uint256 grid_length;
-        //uint8[] game_grid; 
-        //bytes game_grid;
+        uint256 grid_length; 
         string game_grid;
         address player1;
         address player2;
@@ -49,8 +47,7 @@ contract BGCOL {
         game_instances[game_count] = Game(
             _rows,
             _cols,
-            _rows*_cols,
-            //bytes(_seed),
+            _rows*_cols, 
             _seed,
             msg.sender, 
             msg.sender, //player2
@@ -66,204 +63,6 @@ contract BGCOL {
         game_count++; 
     }
  
-
-
-    function runIterations_(uint _gid) private view returns(string memory){ //returns(uint8[] memory) {  
-        
-        //      c   c    c
-        //  r   NW  N   NE
-        //  r   W  CELL  E
-        //  r   SW  S   SE   
-
-        Game memory game = game_instances[_gid];
-        bytes memory game_grid = bytes(game_instances[_gid].game_grid);   
-        bytes memory _res_game_grid;// = bytes(game_instances[_gid].game_grid); // = bytes(game_instances[_gid].game_grid); 
-
-        //Storage : 15450725
-        //MEMORY  : 12669624
-
-        //bool isTopRow;
-        //bool isBottomRow;
-        bool isLeftCol;
-        bool isRightCol;
-
-        uint8[] memory neighs = new uint8[](3);
- 
-        uint256 cell;  
-        uint256 bottom_delimiter = (game.rows-1)*game.cols;
-    
-        uint256 top_delta;
-        uint256 bottom_delta;
-
-        uint8 diff;
-        for(uint i=0; i<game.iterations; ) { 
-            //console.log("iteration: ", i);
-            //console.log('original: ', string(game_grid));
-            _res_game_grid = new bytes(game.grid_length);
-            for(cell=0; cell<game.grid_length; ) {
-                //console.log("cell: ", cell);
-                //neighs[0] = 0;
-                neighs[1] = 0;
-                neighs[2] = 0;
-
-                isLeftCol = cell % game.cols == 0; 
-                isRightCol = (cell+1) % game.cols == 0;
-                
-                
-                if(cell >= game.cols) { //!isTopRow
-                    top_delta = cell-game.cols;
-                    // NW
-                    if(!isLeftCol) {
-                        if(game_grid[top_delta-1] == '1') {
-                            neighs[1]++;
-                        } else if (game_grid[top_delta-1] == '2') {
-                            neighs[2]++; 
-                        }
-                    } 
-                    // N 
-                    if(game_grid[top_delta] == '1') {
-                        neighs[1]++;
-                    } else if (game_grid[top_delta] == '2') {
-                        neighs[2]++; 
-                    }
-                    // NE
-                    if(!isRightCol) {
-                        if(game_grid[top_delta+1] == '1') {
-                            neighs[1]++;
-                        } else if (game_grid[top_delta+1] == '2') {
-                            neighs[2]++; 
-                        }
-                    }
-
-                }
-
-                if(cell < bottom_delimiter) { //!isBottomRow
-                    bottom_delta = cell+game.cols;
-                    // SW
-                    if(!isLeftCol) {
-                        if(game_grid[bottom_delta-1] == '1') {
-                            neighs[1]++;
-                        } else if (game_grid[bottom_delta-1] == '2') {
-                            neighs[2]++; 
-                        }
-                    } 
-                    // S 
-                    if(game_grid[bottom_delta] == '1') {
-                        neighs[1]++;
-                    } else if (game_grid[bottom_delta] == '2') {
-                        neighs[2]++; 
-                    }
-                    // SE
-                    if(!isRightCol) {
-                        if(game_grid[bottom_delta+1] == '1') {
-                            neighs[1]++;
-                        } else if (game_grid[bottom_delta+1] == '2') {
-                            neighs[2]++; 
-                        }
-                    }
-                }
- 
-                // W
-                if(!isLeftCol){ 
-                    if(game_grid[cell-1] == '1') {
-                        neighs[1]++;
-                    } else if (game_grid[cell-1] == '2') {
-                        neighs[2]++; 
-                    }
-                } 
-                // E
-                if(!isRightCol) {
-                    if(game_grid[cell+1] == '1') {
-                        neighs[1]++;
-                    } else if (game_grid[cell+1] == '2') {
-                        neighs[2]++; 
-                    }
-                }
-                 /*
-                console.log("    cell: ", cell); 
-                if(cell < game.cols) { 
-                    console.log("   isTopRow"); 
-                }
-                if(cell >= bottom_delimiter) {
-                    console.log("   isBottomRow"); 
-                }
-                if(isRightCol) {
-                    console.log("   isRiteEdge"); 
-                }
-                if (isLeftCol) {
-                    console.log("   isLeftEdge"); 
-                }
-                console.log("  wNeigh: ", neighs[1]);
-                console.log("  bNeigh: ", neighs[2]);
-                console.log("--------------------------");
-                */
-                
-            
-
-                
-                if(game_grid[cell]=='0') { //EMPTY
-                    if(neighs[1] == 3) { //If has 3 wNei
-                        if(neighs[2] == 3) { //AND 3 bNei
-                            //choose randomly
-                            //_res_game_grid[cell] = uint8(game.randomizer[(i+cell)%32] & 0x01)+1;
-                            //_res_game_grid[cell] = (game.randomizer[(i+cell)%32] & 0x01)+1);
-                            //_res_game_grid[cell] = '1'; 
-                            //console.log(uint8(game.randomizer[(i+cell)%32] & 0x01)+1);
-                            if(uint8(game.randomizer[(i+cell)%32] & 0x01) == 0) { 
-                                _res_game_grid[cell] = '1';
-                            } else {
-                                _res_game_grid[cell] = '2'; 
-                            } 
-                            
-                        } else {            //if bNei != 3 
-                            _res_game_grid[cell] = '1';
-                        }
-                    } else if (neighs[2] == 3) { // if bNei is 3
-                        _res_game_grid[cell] = '2'; 
-                    } else {
-                        _res_game_grid[cell] = '0'; 
-                    }
-                } else  {  
-                        
-                    if(neighs[2] > neighs[1]) {
-                        diff = neighs[2] - neighs[1];
-                    } else {
-                        diff = neighs[1] - neighs[2]; 
-                    }
-                    
-                    if (game_grid[cell]=='1') { //WHITE 
-                        if(diff == 1 && neighs[1] == 1) {
-                            _res_game_grid[cell] = '0';  
-                        }
-                        else if(diff != 2 && diff != 3) {
-                            _res_game_grid[cell] = '0'; 
-                        }  
-                        else {
-                            _res_game_grid[cell] = '1';
-                        }
-                    } else {        
-                        if(diff == 1 && neighs[2] == 1) {
-                            _res_game_grid[cell] = '0';  
-                        }
-                        else if(diff != 2 && diff != 3) {
-                            _res_game_grid[cell] = '0'; 
-                        }
-                        else {
-                            _res_game_grid[cell] = '2';
-                        }
-                    }
-                } 
-                unchecked{cell++;}
-
-            }
-            //console.log(' updated: ', string(_res_game_grid));
-            game_grid = _res_game_grid;
-            unchecked{i++;}
-        }
-        
-        return string(game_grid);
-    } 
-
     function runIterations(uint _gid) private view returns(string memory){ //returns(uint8[] memory) {  
         
         //      c   c    c
@@ -274,18 +73,12 @@ contract BGCOL {
         Game memory game = game_instances[_gid];
         bytes memory game_grid = bytes(game_instances[_gid].game_grid);   
         bytes memory _res_game_grid;// = bytes(game_instances[_gid].game_grid); // = bytes(game_instances[_gid].game_grid); 
-
-        //Storage : 15450725
-        //MEMORY  : 12669624
-        //ARRAY   : 2699773
-
-        //bool isTopRow;
-        //bool isBottomRow;
+ 
         bool isLeftCol;
         bool isRightCol;
 
-        uint8[] memory neighs = new uint8[](3);
-        //mapping(bytes => uint8) memory neighs;
+        uint8 wNeigh;
+        uint8 bNeigh;
 
         uint256 cell;  
         uint256 bottom_delimiter = (game.rows-1)*game.cols;
@@ -294,42 +87,37 @@ contract BGCOL {
         uint256 bottom_delta;
 
         uint8 diff;
-        for(uint i=0; i<game.iterations; ) { 
-            //console.log("iteration: ", i);
-            //console.log('original: ', string(game_grid));
+        for(uint i=0; i<game.iterations; ) {  
             _res_game_grid = new bytes(game.grid_length);
-            for(cell=0; cell<game.grid_length; ) {
-                //console.log("cell: ", cell);
-                //neighs[0] = 0;
-                neighs[1] = 0;
-                neighs[2] = 0;
+            for(cell=0; cell<game.grid_length; ) {  
+                wNeigh = 0;
+                bNeigh = 0; 
 
                 isLeftCol = cell % game.cols == 0; 
-                isRightCol = (cell+1) % game.cols == 0;
-                
+                isRightCol = (cell+1) % game.cols == 0; 
                 
                 if(cell >= game.cols) { //!isTopRow
                     top_delta = cell-game.cols;
                     // NW
                     if(!isLeftCol) { 
                         if(game_grid[top_delta-1] == '1') {
-                            neighs[1]++;
+                            wNeigh++;
                         } else if (game_grid[top_delta-1] == '2') {
-                            neighs[2]++; 
+                            bNeigh++; 
                         }
                     } 
                     // N 
                     if(game_grid[top_delta] == '1') {
-                        neighs[1]++;
+                        wNeigh++;
                     } else if (game_grid[top_delta] == '2') {
-                        neighs[2]++; 
+                        bNeigh++; 
                     }
                     // NE
                     if(!isRightCol) {
                         if(game_grid[top_delta+1] == '1') {
-                            neighs[1]++;
+                            wNeigh++;
                         } else if (game_grid[top_delta+1] == '2') {
-                            neighs[2]++; 
+                            bNeigh++; 
                         }
                     }
 
@@ -340,23 +128,23 @@ contract BGCOL {
                     // SW
                     if(!isLeftCol) {
                         if(game_grid[bottom_delta-1] == '1') {
-                            neighs[1]++;
+                            wNeigh++;
                         } else if (game_grid[bottom_delta-1] == '2') {
-                            neighs[2]++; 
+                            bNeigh++; 
                         }
                     } 
                     // S 
                     if(game_grid[bottom_delta] == '1') {
-                        neighs[1]++;
+                        wNeigh++;
                     } else if (game_grid[bottom_delta] == '2') {
-                        neighs[2]++; 
+                        bNeigh++; 
                     }
                     // SE
                     if(!isRightCol) {
                         if(game_grid[bottom_delta+1] == '1') {
-                            neighs[1]++;
+                            wNeigh++;
                         } else if (game_grid[bottom_delta+1] == '2') {
-                            neighs[2]++; 
+                            bNeigh++; 
                         }
                     }
                 }
@@ -364,49 +152,24 @@ contract BGCOL {
                 // W
                 if(!isLeftCol){ 
                     if(game_grid[cell-1] == '1') {
-                        neighs[1]++;
+                        wNeigh++;
                     } else if (game_grid[cell-1] == '2') {
-                        neighs[2]++; 
+                        bNeigh++; 
                     }
                 } 
                 // E
                 if(!isRightCol) {
                     if(game_grid[cell+1] == '1') {
-                        neighs[1]++;
+                        wNeigh++;
                     } else if (game_grid[cell+1] == '2') {
-                        neighs[2]++; 
+                        bNeigh++; 
                     }
-                }
-                 /*
-                console.log("    cell: ", cell); 
-                if(cell < game.cols) { 
-                    console.log("   isTopRow"); 
-                }
-                if(cell >= bottom_delimiter) {
-                    console.log("   isBottomRow"); 
-                }
-                if(isRightCol) {
-                    console.log("   isRiteEdge"); 
-                }
-                if (isLeftCol) {
-                    console.log("   isLeftEdge"); 
-                }
-                console.log("  wNeigh: ", neighs[1]);
-                console.log("  bNeigh: ", neighs[2]);
-                console.log("--------------------------");
-                */
-                
-            
-
-                
+                } 
+                 
                 if(game_grid[cell]=='0') { //EMPTY
-                    if(neighs[1] == 3) { //If has 3 wNei
-                        if(neighs[2] == 3) { //AND 3 bNei
+                    if(wNeigh == 3) { //If has 3 wNei
+                        if(bNeigh == 3) { //AND 3 bNei
                             //choose randomly
-                            //_res_game_grid[cell] = uint8(game.randomizer[(i+cell)%32] & 0x01)+1;
-                            //_res_game_grid[cell] = (game.randomizer[(i+cell)%32] & 0x01)+1);
-                            //_res_game_grid[cell] = '1'; 
-                            //console.log(uint8(game.randomizer[(i+cell)%32] & 0x01)+1);
                             if(uint8(game.randomizer[(i+cell)%32] & 0x01) == 0) { 
                                 _res_game_grid[cell] = '1';
                             } else {
@@ -416,21 +179,21 @@ contract BGCOL {
                         } else {            //if bNei != 3 
                             _res_game_grid[cell] = '1';
                         }
-                    } else if (neighs[2] == 3) { // if bNei is 3
+                    } else if (bNeigh == 3) { // if bNei is 3
                         _res_game_grid[cell] = '2'; 
                     } else {
                         _res_game_grid[cell] = '0'; 
                     }
                 } else  {  
                         
-                    if(neighs[2] > neighs[1]) {
-                        diff = neighs[2] - neighs[1];
+                    if(bNeigh > wNeigh) {
+                        diff = bNeigh - wNeigh;
                     } else {
-                        diff = neighs[1] - neighs[2]; 
+                        diff = wNeigh - bNeigh; 
                     }
                     
                     if (game_grid[cell]=='1') { //WHITE 
-                        if(diff == 1 && neighs[1] == 1) {
+                        if(diff == 1 && wNeigh == 1) {
                             _res_game_grid[cell] = '0';  
                         }
                         else if(diff != 2 && diff != 3) {
@@ -440,7 +203,7 @@ contract BGCOL {
                             _res_game_grid[cell] = '1';
                         }
                     } else {        
-                        if(diff == 1 && neighs[2] == 1) {
+                        if(diff == 1 && bNeigh == 1) {
                             _res_game_grid[cell] = '0';  
                         }
                         else if(diff != 2 && diff != 3) {
@@ -453,8 +216,7 @@ contract BGCOL {
                 } 
                 unchecked{cell++;}
 
-            }
-            //console.log(' updated: ', string(_res_game_grid));
+            } 
             game_grid = _res_game_grid;
             unchecked{i++;}
         }
